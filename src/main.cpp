a9df0638c59c1649d769a28388d4d6565e0c163c
TEVIN LEWIS <TML22D@FSU.EDU>
2023-02-01 10:32:07 -0500

    lcd test

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+// include libraries that the Arduino IDE would not explicitly require
+#include <Arduino.h>
+#include <Wire.h>
+
+// include the LCD display library
+#include <LiquidCrystal.h>
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD
+  // this happens ONE TIME, since it's in the setup() function
+  lcd.print("Hello, World!");
+}
+
+void loop() {
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print(millis() / 1000);
+}
